046b3a6ee77e0186c4367af195a3da7c046b1450
Rainier Evaristo <dizonrr478@gmail.com>
2025-10-17 23:31:24 +0800

    Add files via upload

---
+++ Organizer.java
@@ -0,0 +1,34 @@
+import java.util.List;
+import java.util.Scanner;
+
+public class Organizer extends User {
+    private String organization; // encapsulated field
+    private Scheduling sched = new Scheduling();
+
+    public Organizer(int role, String name, String password, String organization) {
+        super(role, name, password);
+        this.organization = organization;
+    }
+
+
+    public String getOrganization() {
+        return organization;
+    }
+    
+    public void setOrganization(String organization) {
+        this.organization = organization;
+    }
+
+    public void createEvent(List<Scheduling> allSchedules) {
+        System.out.println("Organization: " + organization);
+        sched.createEvent(allSchedules);
+    }
+
+    public void editEvent() {
+        sched.editEvent();
+    }
+
+    public void viewAllSchedules(List<Scheduling> allSchedules) {
+        sched.displayAllSchedules(allSchedules);
+    }
+}
